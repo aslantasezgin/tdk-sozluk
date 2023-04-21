026d588e74dca37e379910cf1b32f067d76cd795
aslantasezgin <aslantasezginn@gmail.com>
2023-04-22 00:58:37 +0300

    styled system added

--- src/views/search.js
+++ src/views/search.js
@@ -1,18 +1,21 @@
 
 import React from "react"
 import {View, Text, Button} from "react-native"
+import Box from "../components/box";
+import BoxCenter from "../components/box-center";
 
 
 
 function SearchView({navigation}) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <BoxCenter flex={1} justifyContent="center" alignItems="center">
         <Text>Ara</Text>
         <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Detail')}
       />
-      </View>
+    <Box size={20} bg="red" mt={20}></Box>
+      </BoxCenter>
     );
   }
 
