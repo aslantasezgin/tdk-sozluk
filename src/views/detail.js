dac98fee6f831efc1d4764754867d872a76bff8a
aslantasezgin <aslantasezginn@gmail.com>
2023-04-22 00:21:38 +0300

    views created

---
+++ src/views/detail.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+import {View, Text} from "react-native"
+
+function DetailView() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>DetailView</Text>
+      </View>
+    );
+  }
+
+  export default DetailView
---
+++ src/views/favorite.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+import {View, Text} from "react-native"
+
+function FavoriteView() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>FavoriteView!</Text>
+      </View>
+    );
+  }
+
+  export default FavoriteView
